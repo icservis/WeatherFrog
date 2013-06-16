5d67835148ae08ba7347874f09e7e9a0daeb9397
Libor Kucera <codefrog@me.com>
2013-06-16 16:56:36 +0200

    Initial Commit

---
+++ WeatherFrog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeatherFrog
+//
+//  Created by Libor Kučera on 16.06.13.
+//  Copyright (c) 2013 IC Servis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WeatherFrog/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  WeatherFrog
+//
+//  Created by Libor Kučera on 16.06.13.
+//  Copyright (c) 2013 IC Servis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
