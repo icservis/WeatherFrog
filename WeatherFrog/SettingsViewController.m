9405508c4810007dd66326eb6339054b03d0dae4
Libor Kucera <codefrog@me.com>
2013-09-19 11:25:18 +0200

    Settings - removing Facebook

--- WeatherFrog/SettingsViewController.m
+++ WeatherFrog/SettingsViewController.m
@@ -38,8 +38,6 @@
     self.title = NSLocalizedString(@"Settings", nil);
     self.elementsSections = [[UserDefaultsManager sharedDefaults] elementsSections];
         
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fbSessionStateOpened:) name:FbSessionOpenedNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(fbSessionStateClosed:) name:FbSessionClosedNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(defaultsChanged:) name:NSUserDefaultsDidChangeNotification object:nil];
 }
 
@@ -68,152 +66,124 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
-    return self.elementsSections.count + 1;
+    return self.elementsSections.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    if (section == 0) {
-        return 1;
-    } else {
-        NSDictionary* elementsSection = self.elementsSections[section-1];
-        NSArray* elements = [elementsSection objectForKey:@"Elements"];
-        return elements.count;
-    }
+
+    NSDictionary* elementsSection = self.elementsSections[section];
+    NSArray* elements = [elementsSection objectForKey:@"Elements"];
+    return elements.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"Cell";
-    static NSString *FbUserCellIdentifier = @"FbUserCell";
     
-    if (indexPath.section == 0) {
         
-        FbUserCell* cell = (FbUserCell*)[tableView dequeueReusableCellWithIdentifier:FbUserCellIdentifier forIndexPath:indexPath];
-        cell.fbUser = [[self appDelegate] fbUser];        
-        return cell;
+    UITableViewCell* cell = (UITableViewCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UserDefaultsManager* sharedDefaults = [UserDefaultsManager sharedDefaults];
+    
+    NSDictionary* elementsSection = self.elementsSections[indexPath.section];
+    NSArray* elements = [elementsSection objectForKey:@"Elements"];
+    NSDictionary* element = elements[indexPath.row];
+    
+    cell.textLabel.text = [element objectForKey:@"Title"];
+    cell.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    cell.detailTextLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    
+    if ([[element objectForKey:@"Type"] isEqualToString:@"PSToggleSwitchSpecifier"]) {
         
-    } else {
+        BOOL value = NO;
         
-        UITableViewCell* cell = (UITableViewCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-        UserDefaultsManager* sharedDefaults = [UserDefaultsManager sharedDefaults];
-        
-        NSDictionary* elementsSection = self.elementsSections[indexPath.section-1];
-        NSArray* elements = [elementsSection objectForKey:@"Elements"];
-        NSDictionary* element = elements[indexPath.row];
-        
-        cell.textLabel.text = [element objectForKey:@"Title"];
-        cell.textLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-        cell.detailTextLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
-        
-        if ([[element objectForKey:@"Type"] isEqualToString:@"PSToggleSwitchSpecifier"]) {
-            
-            BOOL value = NO;
-            
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsFetchForecastInBackground]) {
-                value = [sharedDefaults fetchForecastInBackground];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsShareLocationAndForecast]) {
-                value = [sharedDefaults shareLocationAndForecast];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsSoundEffects]) {
-                value = [sharedDefaults soundEffects];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsOnScreenHelp]) {
-                value = [sharedDefaults onScreenHelp];
-            }
-            
-            if (value == YES) {
-                cell.accessoryType = UITableViewCellAccessoryCheckmark;
-            } else {
-                cell.accessoryType = UITableViewCellAccessoryNone;
-            }
-            
-            cell.detailTextLabel.text = nil;
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsFetchForecastInBackground]) {
+            value = [sharedDefaults fetchForecastInBackground];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsShareLocationAndForecast]) {
+            value = [sharedDefaults shareLocationAndForecast];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsSoundEffects]) {
+            value = [sharedDefaults soundEffects];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsOnScreenHelp]) {
+            value = [sharedDefaults onScreenHelp];
         }
         
-        if ([[element objectForKey:@"Type"] isEqualToString:@"PSMultiValueSpecifier"]) {
-            
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsDisplayMode]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults displayMode] forKey:DefaultsDisplayMode];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsLocationGeocoderAccuracy]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults locationGeocoderAccuracy] forKey:DefaultsLocationGeocoderAccuracy];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsLocationGeocoderTimeout]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults locationGeocoderTimeout] forKey:DefaultsLocationGeocoderTimeout];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastAccuracy]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastAccuracy] forKey:DefaultsForecastAccuracy];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastValidity]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastValidity] forKey:DefaultsForecastValidity];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitTemperature]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitTemperature] forKey:DefaultsForecastUnitTemperature];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitWindspeed]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitWindspeed] forKey:DefaultsForecastUnitWindspeed];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitPrecipitation]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitPrecipitation] forKey:DefaultsForecastUnitPrecipitation];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitPressure]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitPressure] forKey:DefaultsForecastUnitPressure];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitAltitude]) {
-                cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitAltitude] forKey:DefaultsForecastUnitAltitude];
-            }
-            
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        if (value == YES) {
+            cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryNone;
         }
         
-        if ([[element objectForKey:@"Type"] isEqualToString:@"PSSliderSpecifier"]) {
-            
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsNotifications]) {
-                
-                BOOL determiningValue = [sharedDefaults fetchForecastInBackground];
-                
-                if (determiningValue == NO) {
-                    cell.detailTextLabel.text = NSLocalizedString(@"Disabled", nil);
-                    cell.accessoryType = UITableViewCellAccessoryNone;
-                } else {
-                    cell.detailTextLabel.text = [sharedDefaults titleOfSliderValue:[sharedDefaults notifications] forKey:DefaultsNotifications];
-                    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-                }
-            }
-        }
-        
-        return cell;
+        cell.detailTextLabel.text = nil;
     }
     
-    return nil;
+    if ([[element objectForKey:@"Type"] isEqualToString:@"PSMultiValueSpecifier"]) {
+        
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsDisplayMode]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults displayMode] forKey:DefaultsDisplayMode];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsLocationGeocoderAccuracy]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults locationGeocoderAccuracy] forKey:DefaultsLocationGeocoderAccuracy];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsLocationGeocoderTimeout]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults locationGeocoderTimeout] forKey:DefaultsLocationGeocoderTimeout];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastAccuracy]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastAccuracy] forKey:DefaultsForecastAccuracy];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastValidity]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastValidity] forKey:DefaultsForecastValidity];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitTemperature]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitTemperature] forKey:DefaultsForecastUnitTemperature];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitWindspeed]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitWindspeed] forKey:DefaultsForecastUnitWindspeed];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitPrecipitation]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitPrecipitation] forKey:DefaultsForecastUnitPrecipitation];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitPressure]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitPressure] forKey:DefaultsForecastUnitPressure];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsForecastUnitAltitude]) {
+            cell.detailTextLabel.text = [sharedDefaults titleOfMultiValue:[sharedDefaults forecastUnitAltitude] forKey:DefaultsForecastUnitAltitude];
+        }
+        
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    if ([[element objectForKey:@"Type"] isEqualToString:@"PSSliderSpecifier"]) {
+        
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsNotifications]) {
+            
+            BOOL determiningValue = [sharedDefaults fetchForecastInBackground];
+            
+            if (determiningValue == NO) {
+                cell.detailTextLabel.text = NSLocalizedString(@"Disabled", nil);
+                cell.accessoryType = UITableViewCellAccessoryNone;
+            } else {
+                cell.detailTextLabel.text = [sharedDefaults titleOfSliderValue:[sharedDefaults notifications] forKey:DefaultsNotifications];
+                cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            }
+        }
+    }
+    
+    return cell;
 }
 
 - (NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    if (section == 0) {
-        
-        return nil;
-        
-    } else {
-        
-        NSDictionary* elementsSection = self.elementsSections[section-1];
-        return [elementsSection objectForKey:@"Title"];
-    }
+    NSDictionary* elementsSection = self.elementsSections[section];
+    return [elementsSection objectForKey:@"Title"];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if (indexPath.section == 0) {
-        
-        return 64.0f;
-        
-    } else {
-        
-        return 44.0f;
-    }
+    return 44.0f;
 }
 
 #pragma mark - UITableViewDelegate
@@ -221,56 +191,48 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-
-    if (indexPath.section > 0) {
         
-        UserDefaultsManager* sharedDefaults = [UserDefaultsManager sharedDefaults];
+    UserDefaultsManager* sharedDefaults = [UserDefaultsManager sharedDefaults];
+    
+    NSDictionary* elementsSection = self.elementsSections[indexPath.section];
+    NSArray* elements = [elementsSection objectForKey:@"Elements"];
+    NSDictionary* element = elements[indexPath.row];
+    
+    if ([[element objectForKey:@"Type"] isEqualToString:@"PSToggleSwitchSpecifier"]) {
         
-        NSDictionary* elementsSection = self.elementsSections[indexPath.section-1];
-        NSArray* elements = [elementsSection objectForKey:@"Elements"];
-        NSDictionary* element = elements[indexPath.row];
         
-        if ([[element objectForKey:@"Type"] isEqualToString:@"PSToggleSwitchSpecifier"]) {
-            
-            
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsFetchForecastInBackground]) {
-                BOOL fetchForecastInBackground = [sharedDefaults fetchForecastInBackground];
-                [[UserDefaultsManager sharedDefaults] setFetchForecastInBackground:!fetchForecastInBackground];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsShareLocationAndForecast]) {
-                BOOL shareLocationAndForecast = [sharedDefaults shareLocationAndForecast];
-                [[UserDefaultsManager sharedDefaults] setShareLocationAndForecast:!shareLocationAndForecast];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsSoundEffects]) {
-                BOOL soundEffects = [sharedDefaults soundEffects];
-                [[UserDefaultsManager sharedDefaults] setSoundEffects:!soundEffects];
-            }
-            if ([[element objectForKey:@"Key"] isEqualToString:DefaultsOnScreenHelp]) {
-                BOOL onScreenHelp = [sharedDefaults onScreenHelp];
-                [[UserDefaultsManager sharedDefaults] setOnScreenHelp:!onScreenHelp];
-            }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsFetchForecastInBackground]) {
+            BOOL fetchForecastInBackground = [sharedDefaults fetchForecastInBackground];
+            [[UserDefaultsManager sharedDefaults] setFetchForecastInBackground:!fetchForecastInBackground];
         }
-        
-        if ([[element objectForKey:@"Type"] isEqualToString:@"PSMultiValueSpecifier"]) {
-            
-            [self performSegueWithIdentifier:@"showMultiValue" sender:[element objectForKey:@"Key"]];
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsShareLocationAndForecast]) {
+            BOOL shareLocationAndForecast = [sharedDefaults shareLocationAndForecast];
+            [[UserDefaultsManager sharedDefaults] setShareLocationAndForecast:!shareLocationAndForecast];
         }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsSoundEffects]) {
+            BOOL soundEffects = [sharedDefaults soundEffects];
+            [[UserDefaultsManager sharedDefaults] setSoundEffects:!soundEffects];
+        }
+        if ([[element objectForKey:@"Key"] isEqualToString:DefaultsOnScreenHelp]) {
+            BOOL onScreenHelp = [sharedDefaults onScreenHelp];
+            [[UserDefaultsManager sharedDefaults] setOnScreenHelp:!onScreenHelp];
+        }
+    }
+    
+    if ([[element objectForKey:@"Type"] isEqualToString:@"PSMultiValueSpecifier"]) {
         
-        if ([[element objectForKey:@"Type"] isEqualToString:@"PSSliderSpecifier"]) {
-                        
-            if ([sharedDefaults fetchForecastInBackground] == YES) {
-                [self performSegueWithIdentifier:@"showSlider" sender:[element objectForKey:@"Key"]];
-            }
+        [self performSegueWithIdentifier:@"showMultiValue" sender:[element objectForKey:@"Key"]];
+    }
+    
+    if ([[element objectForKey:@"Type"] isEqualToString:@"PSSliderSpecifier"]) {
+                    
+        if ([sharedDefaults fetchForecastInBackground] == YES) {
+            [self performSegueWithIdentifier:@"showSlider" sender:[element objectForKey:@"Key"]];
         }
     }
 }
 
 #pragma mark - NSnotification
-
-- (void)fbSessionStateOpened:(NSNotification*)notification
-{
-    [self.tableView reloadData];
-}
 
 - (void)fbSessionStateClosed:(NSNotification*)notification
 {
