f46055fa11285371a4551e25678b5c759e12164b
Libor Kucera <codefrog@me.com>
2016-07-03 21:10:15 +0200

    Today Extension

---
+++ WeatherFrog/WeatherToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  WeatherToday
+//
+//  Created by Libor Kučera on 16.04.16.
+//  Copyright © 2016 IC Servis, s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
